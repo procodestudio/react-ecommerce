3c47c2cc81425d929e9d126fa7ac1559773580f6
Sergio Sampaio <sergiosampaio@Sergios-MacBook-Pro.local>
2020-05-21 11:41:30 -0300

    Basic homepage component with styling

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import './styles.css';
+import Homepage from '../homepage';
+import './styles.scss';
 
-const App = () => <div>E-commerce App</div>;
+const App = () => <Homepage />;
 
 export default App;
---
+++ src/components/homepage/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.scss';
+
+const Homepage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="subtitle">Shop now</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Homepage;
