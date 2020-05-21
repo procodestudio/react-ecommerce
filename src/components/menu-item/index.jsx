f5d8e5f4dcf6eeb4dbe74dc8eee4d4d89de48a8f
Sergio Sampaio <sergiosampaio@Sergios-MacBook-Pro.local>
2020-05-21 13:20:03 -0300

    Finished styling on menu items component

--- src/components/menu-item/index.jsx
+++ src/components/menu-item/index.jsx
@@ -3,14 +3,15 @@ import PropTypes from 'prop-types';
 import './styles.scss';
 
 const MenuItem = ({ data: { title, imageUrl, size } }) => (
-  <div
-    style={{
-      backgroundImage: `url(${imageUrl})`,
-    }}
-    className={`menu-item ${size}`}
-  >
+  <div className={`menu-item ${size}`}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
     <div className="content">
-      <h1 className="title">{ title }</h1>
+      <h1 className="title">{ title.toUpperCase() }</h1>
       <span className="subtitle">Shop now</span>
     </div>
   </div>
