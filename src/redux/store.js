b26104df4dc669f2ec067dbe8fe61ebd7f9deacd
Sergio Sampaio <sergiosampaio@Sergios-MacBook-Pro.local>
2020-05-25 02:05:36 -0300

    Added persistence to shopping cart

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
+import { persistStore } from 'redux-persist';
 import rootReducer from './reducers/index';
 
 const logger = createLogger({ collapsed: true });
 const middlewares = [logger];
 const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
 const store = createStoreWithMiddleware(rootReducer);
+const persistor = persistStore(store);
 
-export default store;
+export { store, persistor };
